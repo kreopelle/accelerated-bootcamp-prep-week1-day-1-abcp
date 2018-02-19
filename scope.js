fbfc6b1a6c84359b02f1372573aef62216cc83ab
Kayla Reopelle <kreopelle@gmail.com>
2018-02-19 17:35:15 +0000

    automatically backed up by learn

--- scope.js
+++ scope.js
@@ -11,18 +11,3 @@ function dogInfo(){
 setSpeciesVariable();
 dogInfo();
 
-function setSpeciesVariable() {
-  species = "cat"
-}
-
-function catInfo() {
-  var name = "Felix"
-  var mood = "sleepy"
-  console.log("Species is " + species);
-  console.log("Name is " + name);
-  console.log("Mood is " + mood)
-}
-
-setSpeciesVariable();
-catInfo();
-
